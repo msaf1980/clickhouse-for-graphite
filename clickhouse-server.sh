5ae02d74e5520f78c7d7e4196ef0209e63cb402d
msaf1980 <msaf1980@gmail.com>
2020-10-20 23:39:57 +0500

    server: add ports

--- clickhouse-server.sh
+++ clickhouse-server.sh
@@ -7,6 +7,7 @@ mkdir log data
 
 docker run -d --name clickhouse-graphite \
      --ulimit nofile=262144:262144 \
+     -p 127.0.0.1:8123:8123 -p 127.0.0.1:9000:9000 \
      -v ${PWD}/log:/var/log/clickhouse-server \
      -v ${PWD}/data:/var/lib/clickhouse \
      -v ${PWD}/rollup.xml:/etc/clickhouse-server/config.d/rollup.xml \
